3a6bf0c98adf7d8faa2ff599402891ec25222814
Ania <an.pasieczna@gmail.com>
2022-11-09 17:15:41 +0100

    errol modal style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
+import { useState } from "react";
+import uniqid from "uniqid";
 function App() {
+  const [userList, setUserList] = useState([]);
+  const addUserHaldler = (userName, userAge) => {
+    //As parameterrs we add what we would like to receive from the child
+    setUserList((prevUserList) => {
+      return [...prevUserList, { id: uniqid(), name: userName, age: userAge }];
+    });
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHaldler} />
+      <UserList users={userList} />
     </div>
   );
 }
